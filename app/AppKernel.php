1e80c13debd2441549caf8acde2388d9fd79a3f7
Behnam Hatami <behnamhatami@gmail.com>
2013-05-21 22:49:25 +0430

    User Bundle Added.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new HW3\NewsBundle\NewsBundle(),
+            new Hw3\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Hw3/UserBundle/UserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hw3\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
